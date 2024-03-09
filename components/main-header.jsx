dca4711195ab51f8b5b7e7f018b86a4fe5f6ad9c
Sameh <sameh.abdelhaleem95@gmail.com>
2024-03-09 15:28:07 +0200

    add MainHeader acontent

---
+++ components/main-header.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import logo from "../assets/logo.png";
+const MainHeader = () => {
+  return (
+    <header>
+      <Link href={"/"}>
+        <img src={logo.src} alt="a plate with food on it" />
+        NextLevel Food
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href={"/meals"}>Browse Meals</Link>
+          </li>
+          <li>
+            <Link href={"/community"}>Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
