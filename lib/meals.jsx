cae55490fba6242f6b5d16ff0540748f886658aa
Sameh <sameh.abdelhaleem95@gmail.com>
2024-03-09 22:39:46 +0200

    fetch single meal details and populate details page with it's data

--- lib/meals.jsx
+++ lib/meals.jsx
@@ -3,6 +3,8 @@ const db = sql("meals.db");
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  throw new Error("loading meals failed");
   return db.prepare("SELECT * FROM meals").all();
+}
+export function getMeal(slug) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
 }
