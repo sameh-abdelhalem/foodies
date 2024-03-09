701fa178dd30c16b9278be8e3993bbb241ed5411
Sameh <sameh.abdelhaleem95@gmail.com>
2024-03-09 21:25:44 +0200

    add loading page for meals

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
-const Meals = () => {
+import { getMeals } from "@/lib/meals";
+const Meals = async () => {
+  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +19,7 @@ const Meals = () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
