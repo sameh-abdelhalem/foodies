4549efb9b296463b1ac78aea05cec3b3d072fbf6
Sameh <sameh.abdelhaleem95@gmail.com>
2024-03-10 22:34:44 +0200

    test for deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/foodies",
-  output: "export", // <=== enables static exports
-  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
